6c4af09b1d024602c6e4d82e7ba37be7b0fe291a
samuelklein <samuk.klein@gmail.com>
2020-09-05 22:13:02 -0300

    clean code

--- product.core/src/main/java/com/espressif/sample/product/core/ui/handler/NewProductHandler.java
+++ product.core/src/main/java/com/espressif/sample/product/core/ui/handler/NewProductHandler.java
@@ -5,12 +5,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import com.espressif.sample.product.core.data.ProductDAO;
 import com.espressif.sample.product.core.ui.dialog.NewProductDialog;
 
 public class NewProductHandler extends AbstractHandler {
-	
-	private ProductDAO dao = ProductDAO.getInstance(); 
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
--- product.core/src/main/java/com/espressif/sample/product/core/ui/handler/SearchProductHandler.java
+++ product.core/src/main/java/com/espressif/sample/product/core/ui/handler/SearchProductHandler.java
@@ -3,24 +3,17 @@ package com.espressif.sample.product.core.ui.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
-
-import com.espressif.sample.product.core.data.ProductDAO;
 
 public class SearchProductHandler extends AbstractHandler {
 
-	private ProductDAO dao = ProductDAO.getInstance();
-	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
 		try {
 			var problemView = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("com.espressif.sample.product.core.ui.view.ViewSeachProduct");
 			problemView.setFocus();
 		} catch (Exception e) {
+			//TODO
 		}
 		return null;
 	}
